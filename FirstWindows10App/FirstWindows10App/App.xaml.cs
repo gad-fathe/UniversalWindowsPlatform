481f0b39d16fc1d7ce06e975f719f5ebb63aebe1
Daniel Krzyczkowski <daniel.krzyczkowski@hotmail.com>
2016-01-29 14:24:41 +0100

    Create your first Universal Windows 10 App.

--- FirstWindows10App/FirstWindows10App/App.xaml.cs
+++ FirstWindows10App/FirstWindows10App/App.xaml.cs
@@ -17,15 +17,9 @@ using Windows.UI.Xaml.Navigation;
 
 namespace FirstWindows10App
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
+
     sealed partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
@@ -35,11 +29,6 @@ namespace FirstWindows10App
             this.Suspending += OnSuspending;
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
 
