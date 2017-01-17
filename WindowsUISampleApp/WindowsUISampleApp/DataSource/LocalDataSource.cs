24601ab94f01dde075a19993a0900ac7fff74681
Daniel Krzyczkowski <daniel.krzyczkowski@hotmail.com>
2017-01-17 17:46:31 +0100

    Paralaxed ListView changed.

--- WindowsUISampleApp/WindowsUISampleApp/DataSource/LocalDataSource.cs
+++ WindowsUISampleApp/WindowsUISampleApp/DataSource/LocalDataSource.cs
@@ -42,11 +42,10 @@ namespace WindowsUISampleApp.DataSource
         public LocalDataSource()
         {
             Items = new ObservableCollection<Thumbnail>();
-            Items.Add(new Thumbnail("Sample 1", PREFIX_URL + "sample1.png", "24mm f/4.0 1/2500 ISO 200"));
-            Items.Add(new Thumbnail("Sample 2", PREFIX_URL + "sample2.png", "24mm f/8.0 1/2000 ISO 100"));
-            Items.Add(new Thumbnail("Sample 3", PREFIX_URL + "sample3.png", "24mm f/8.0 1/640 ISO 100"));
-            Items.Add(new Thumbnail("Sample 4", PREFIX_URL + "sample4.png", "70mm f/2.8 1/8000 ISO 400"));
-            Items.Add(new Thumbnail("Sample 5", PREFIX_URL + "sample5.png", "70mm f/2.8 1/5000 ISO 400"));
+            Items.Add(new Thumbnail("Sample 1", PREFIX_URL + "sample2.png", "24mm f/8.0 1/2000 ISO 100"));
+            Items.Add(new Thumbnail("Sample 2", PREFIX_URL + "sample3.png", "24mm f/8.0 1/640 ISO 100"));
+            Items.Add(new Thumbnail("Sample 3", PREFIX_URL + "sample4.png", "70mm f/2.8 1/8000 ISO 400"));
+            Items.Add(new Thumbnail("Sample 4", PREFIX_URL + "sample5.png", "70mm f/2.8 1/5000 ISO 400"));
         }
 
         public ObservableCollection<Thumbnail> Items
