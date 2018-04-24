69cecfd3e2ab175bd5974b1f1c827a940e017236
Daniel <krzyczkowski.daniel@op.pl>
2018-04-24 21:11:43 +0200

    AzurePushNotificationHubApp project commited.

---
+++ AzurePushNotificationHubApp/PushNotificationsApp.UWP/Model/DeviceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushNotificationsApp.UWP.Model
+{
+    public class DeviceRegistration
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public MobilePlatform Platform { get; set; }
+        public string Handle { get; set; }
+        public string[] Tags { get; set; }
+    }
+}
---
+++ AzurePushNotificationHubApp/PushNotificationsApp.UWP/Model/MobilePlatform.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushNotificationsApp.UWP.Model
+{
+    public enum MobilePlatform
+    {
+        wns, apns, gcm
+    }
+}
