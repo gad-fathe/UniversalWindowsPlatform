945412cc7febbabffa07b90aeee02e0fc65fe6c0
Daniel <krzyczkowski.daniel@op.pl>
2019-08-01 07:14:20 +0200

    FileAccessWithEnvironmentVariables app added

---
+++ FileAccessWithEnvironmentVariables/FileAccessWithEnvironmentVariables.UWP/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using FileAccessWithEnvironmentVariables.UWP.Services;
+using FileAccessWithEnvironmentVariables.UWP.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FileAccessWithEnvironmentVariables.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly IFileService _fileService;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _fileService = new FileService();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+           var fileContent = await _fileService.LoadFileContentUsingPathFromEnvironemntVariable("Test_File_Path");
+            FileContentTextBlock.Text = fileContent;
+        }
+    }
+}
---
+++ FileAccessWithEnvironmentVariables/FileAccessWithEnvironmentVariables.UWP/Services/FileService.cs
@@ -0,0 +1,34 @@
+﻿using FileAccessWithEnvironmentVariables.UWP.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace FileAccessWithEnvironmentVariables.UWP.Services
+{
+    public class FileService : IFileService
+    {
+        public async Task<string> LoadFileContentUsingPathFromEnvironemntVariable(string environmentVariableName)
+        {
+            if (string.IsNullOrEmpty(environmentVariableName))
+            {
+                throw new ArgumentNullException("Please provide the name of environment variable");
+            }
+
+            var filePathFromEnvironmentVariable = Environment.GetEnvironmentVariable(environmentVariableName);
+
+            if (string.IsNullOrEmpty(filePathFromEnvironmentVariable))
+            {
+                throw new ArgumentNullException("Please provide the value of environment variable");
+            }
+
+            var textFileFromExternalPath = await StorageFile.GetFileFromPathAsync(filePathFromEnvironmentVariable);
+
+            var fileContent = await FileIO.ReadTextAsync(textFileFromExternalPath);
+
+            return fileContent;
+        }
+    }
+}
